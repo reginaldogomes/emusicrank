479468317f49a06f77c1ad73e1dfcac7617a2d8a
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-10-20 00:00:26 -0300

    fix: bug playlist

--- backend/src/services/SpotifyService.ts
+++ backend/src/services/SpotifyService.ts
@@ -1,16 +1,25 @@
-// src/services/SpotifyService.ts
-
 import spotifyApi from '../config/spotifyApi';
 import { ISpotifyService } from '../interfaces/ISpotifyService';
 
 class SpotifyService implements ISpotifyService {
     async getTopTracksByGenre(genre: string) {
-        // Obtém os dados do Spotify com base no gênero fornecido
+        // Obtém as playlists relacionadas ao gênero fornecido no Spotify
         try {
-            const response = await spotifyApi.getGenrePlaylists(genre);
-            return response.body.playlists.items; // Retorna as músicas
-        } catch (error) {
-            throw new Error('Erro ao obter músicas: ' + error.message);
+            // Pesquisar playlists usando o gênero como termo de busca
+            const response = await spotifyApi.searchPlaylists(genre);
+            const playlists = response.body.playlists;
+
+            // Verifica se playlists está definido e se contém itens
+            if (playlists && playlists.items.length > 0) {
+                const playlistId = playlists.items[0].id; // Pegando a primeira playlist como exemplo
+                const tracksResponse = await spotifyApi.getPlaylistTracks(playlistId);
+                return tracksResponse.body.items; // Retorna as faixas da playlist
+            }
+
+            throw new Error('Nenhuma playlist encontrada para o gênero especificado.');
+        } catch (error: any) {
+            const errorMessage = error?.message || 'Erro desconhecido ao obter músicas.';
+            throw new Error(errorMessage);
         }
     }
 }
